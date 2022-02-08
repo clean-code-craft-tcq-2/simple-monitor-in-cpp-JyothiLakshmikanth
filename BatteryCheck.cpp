91cf84f75f751ff460462f3d70632642acec6027
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-02-08 16:06:24 +0530

    Update and rename checker.cpp to BatteryCheck.cpp

--- BatteryCheck.cpp
+++ BatteryCheck.cpp
@@ -1,29 +1,13 @@
 #include <assert.h>
 #include <iostream>
 #include <string>
+using namespace std;
 
 #define MIN_TEMP 0
 #define MAX_TEMP 45
 #define MIN_SOC 20
 #define MAX_SOC 80
 #define CHARGE_RATE 0.8
-
-using namespace std;
-
-// bool batteryIsOk(float temperature, float soc, float chargeRate) {
-//   if(temperature < 0 || temperature > 45) {
-//     cout << "Temperature out of range!\n";
-//     return false;
-//   } else if(soc < 20 || soc > 80) {
-//     cout << "State of Charge out of range!\n";
-//     return false;
-//   } else if(chargeRate > 0.8) {
-//     cout << "Charge Rate out of range!\n";
-//     return false;
-//   }
-//   return true;
-// }
-
 
 class Battery
 {
@@ -33,18 +17,23 @@ class Battery
     bool checkTemperatureRange(float temperatue, void(*fnprint)(string));
     bool checkStateOfCharegeRange( float soc, void(*fnprint)(string));
     bool checkChargeRateRange(float rate, void(*fnprint)(string));
-    static void print(string output);
+    static void printWarning(string output);
 };
 
-void Battery::print(string output)
+void Battery::printWarning(string output)
 {
     cout << output.append(" is OUT OF RANGE !!!") << ::std::endl;
 }
 
 bool Battery::isBatteryOK(float temperature, float soc, float chargeRate)
 {
-    return checkTemperatureRange(temperature, &print) && checkStateOfCharegeRange(soc, &print)
-    && checkChargeRateRange(chargeRate, &print);
+    bool isOK = false;
+    if(checkTemperatureRange(temperature, &printWarning) && checkStateOfCharegeRange(soc, &printWarning) && checkChargeRateRange(chargeRate, &printWarning))
+    {
+        isOK = true;
+        cout << "ALL IS WELL !!!!" << ::std::endl;
+    }
+    return isOK;
 }
 
 bool Battery::checkTemperatureRange(float temperatue, void(*fnprint)(string))
@@ -79,14 +68,13 @@ bool Battery::checkChargeRateRange(float rate, void(*fnprint)(string))
 
 void testBattery(float temperature, float soc, float chargeRate, bool expected)
 {
-    Battery b;
-    assert(b.isBatteryOK(temperature, soc, chargeRate) == expected);
+    Battery battery;
+    assert(battery.isBatteryOK(temperature, soc, chargeRate) == expected);
 }
 
 int main() {
-  testBattery(58, 90, 0.9, false);
-//   assert(batteryIsOk(50, 85, 0) == false);
-// Battery b;
-// bool result = b.isBatteryOK(250, 70, 0.8);
-// printf("result - %d", result);
+  testBattery(58, 60, 0.8, false);
+  testBattery(40, 80, 0.7, false);
+  testBattery(40, 60, 0.8, false);
+  testBattery(40, 60, 0.7, true);
 }
